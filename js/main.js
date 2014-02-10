1ba940c97120c91af0c786f1a8be8ce6c1bbf20d
maxim1994 <nok6233@gmail.com>
2014-02-10 16:03:01 +0200

    edit10.02.14

--- js/main.js
+++ js/main.js
@@ -6,36 +6,11 @@ $(document).ready( function(){
     $('.bxslider').bxSlider();
     
     //menu
-    $('.a-nav',this).toggle(function(){
-        $('ul.sub').slideDown(300);
-        $(this).css({
-         '-webkit-box-shadow': '0px 10px 6px -11px rgba(0,0,0,0.75)',
-        '-moz-box-shadow': '0px 10px 6px -11px rgba(0,0,0,0.75)',
-        'box-shadow': '0px 10px 6px -11px rgba(0,0,0,0.75)',
-        'color': '#ffffff',
-        'text-shadow': '-1px -1px 30px rgba(255, 255, 255, 0.72)',
-        'background': 'url(./img/img-noise-170x53.png)',
-    });}
-    ,function(){
-        $('ul.sub').slideUp(300);
-        $(this).css({
-            'background': 'none',
-            'color': '#454444'
-        });
+    $("ul.sub").prev('a').click(function(){
+        $(this).next('.sub').slideDown(250);
+        return false;
     });
+   
   
-     $('.bg-kip').click(function(){
-        $('.sub').slideUp(500)
-        $('.a-nav').css({
-            'background': 'none',
-            'color': '#454444'
-        });
-        });
-        
-  //radio button 
-  $('input').iCheck({
-    checkboxClass: 'icheckbox_polaris',
-    radioClass: 'iradio_polaris',
-  }); 
 
 });//end ready 
